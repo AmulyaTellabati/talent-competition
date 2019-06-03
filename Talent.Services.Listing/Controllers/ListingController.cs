84e952cc694d4fe288f7b30ba6f55b476bbe31aa
AmulyaTellabati <amulya.tellabati@gmail.com>
2019-06-03 16:43:27 +1000

    Project check in
    Project check in

--- Talent.Services.Listing/Controllers/ListingController.cs
+++ Talent.Services.Listing/Controllers/ListingController.cs
@@ -177,20 +177,21 @@ namespace Talent.Services.Listing.Controllers
                     sortedJobs = sortedJobs.Where(x => x.Status != JobStatus.Active);
                 }
 
-                if(!showClosed)
+                if (!showClosed)
                 {
                     sortedJobs = sortedJobs.Where(x => x.Status != JobStatus.Closed);
                 }
+
 
                 if (!showExpired)
                 {
                     sortedJobs = sortedJobs.Where(x => x.ExpiryDate >= DateTime.UtcNow);
                 }
 
-                if (!showUnexpired)
-                {
-                    sortedJobs = sortedJobs.Where(x => x.ExpiryDate < DateTime.UtcNow);
-                }
+                //if (!showUnexpired)
+                //{
+                //    sortedJobs = sortedJobs.Where(x => x.ExpiryDate < DateTime.UtcNow);
+                //}
 
                 //TODO Draft not implemented yet
                 //if (!showDraft)
